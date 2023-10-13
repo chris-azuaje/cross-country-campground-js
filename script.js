fbb9ceb4c52bb21d89ef5594faca5f236675046a
Chris Azuaje <christopherazuaje@gmail.com>
2023-10-13 08:35:21 -0400

    Committing changes

--- script.js
+++ script.js
@@ -138,6 +138,18 @@ prevNextIcon.forEach((icon) => {
   });
 });
 
+function sendEmail() {
+  Email.send({
+    Host: 'smtp.elasticemail.com',
+    Username: 'username',
+    Password: 'password',
+    To: 'them@website.com',
+    From: 'you@isp.com',
+    Subject: 'This is the subject',
+    Body: 'And this is the body',
+  }).then((message) => alert(message));
+}
+
 // ---- Reveal Nav ----
 
 /*
